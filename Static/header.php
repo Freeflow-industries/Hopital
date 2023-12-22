e2cc5e7990cbc95203721ce890e572ef12982ff2
Freeflow-industries <141038136+Freeflow-industries@users.noreply.github.com>
2023-12-22 11:29:42 +0000

    Add files via upload

---
+++ Static/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="title">
+            <a href="index.php"><span class="logo-set"></span></a>
+            <span>Healthy Core</span> 
+        </div>
+        <nav>
+            <li><a href="./../index.php">Home</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="./../contact.php">Contact Us</a></li>
+            <li><a href="#">Help</a></li>
+            <li><a href="#">Blogs</a></li>
+        </nav>
+        <div class="connexion">
+            <span class="one"><a href="#">Sign Up</a></span>
+            <span class="two"><a href="#">Log In</a></span>
+        </div>
+    </header>
